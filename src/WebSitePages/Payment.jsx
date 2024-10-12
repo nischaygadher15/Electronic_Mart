409e4e6cd69a26d58fa3f6be3d71e7ed77a4f56c
nischay gadher <nischaygadher15@gmail.com>
2024-10-12 19:09:55 +0530

    Pyment page done.

--- src/WebSitePages/Payment.jsx
+++ src/WebSitePages/Payment.jsx
@@ -1,7 +1,102 @@
 import React, { useEffect } from "react";
 import style from "./Payment.module.css";
+import payPal from "../assets/paypal.png";
 
 const Payment = () => {
+  let popularBanks = [
+    "Syndicate Bank",
+    "Bank of Baroda",
+    "Canara Bank",
+    "ICICI Bank",
+    "State Bank Of India",
+  ];
+
+  let otherBanks = [
+    "Abhyudaya Co-operative Bank",
+    "Ahmedabad District Cooperative Bank",
+    "Andhra Pragathi Grameena Bank",
+    "APNA Sahakari Bank ltd",
+    "Andhra Pradesh Grameena Vikas Bank - IMPS",
+    "AIRTEL PAYMENTS BANK LIMITED",
+    "Arvind Sahakari Bank Ltd",
+    "Aryavart Bank",
+    "AU SMALL FINANCE BANK",
+    "Axis Bank",
+    "Bandhan Bank limited",
+    "Baroda Gujarat Gramin Bank",
+    "Bangiya Gramin Vikash Bank",
+    "THE BHARAT CO OPERATIVE BANK MUMBAI LTD",
+    "The Banaskantha District Central Co-Op. Bank Ltd.",
+    "Baroda Rajasthan Kshetriya Gramin Bank",
+    "Bhagini Nivedita Sahakari Bank Ltd,Pune",
+    "Bank of Baroda",
+    "Bank Of India",
+    "Bank of Maharashtra",
+    "Baroda U.P. Bank (Erstwhile Baroda Uttar Pradesh Gramin Bank)",
+    "Central Bank of India",
+    "CHAITANYA GODAVARI GRAMEENA BANK",
+    "CHHATTISGARH RAJYA GRAMIN BANK",
+    "Canara Bank",
+    "The Cosmos Co-Operative Bank Ltd",
+    "City Union Bank",
+    "DBS Bank India Limited",
+    "DCB Bank Ltd",
+    "Dhanalakshmi Bank",
+    "Dombivli Nagarik Sahakari Bank",
+    "ESAF SMALL FINANCE BANK LTD",
+    "EQUITAS SMALL FINANCE BANK",
+    "Federal Bank",
+    "Fino Payments Bank Limited FIP",
+    "AU Small Finance Bank (Erstwhile Fincare)",
+    "G P Parsik Sahakari Bank Ltd",
+    "The Gujarat State Co-op Bank Ltd",
+    "The Gayatri Co-operative Urban Bank Ltd.",
+    "HDFC Bank",
+    "HIMACHAL PRADESH GRAMIN BANK",
+    "The Himachal Pradesh State Cooperative Bank Ltd.",
+    "HSBC Bank",
+    "The Hasti Co-op. Bank Ltd.",
+    "Indusind Bank M2P",
+    "ICICI Bank",
+    "IDBI Bank",
+    "IDFC First Bank Ltd",
+    "Indusind Bank",
+    "Indian Bank",
+    "Indian Overseas Bank",
+    "INDIA POST PAYMENTS BANK LIMITED",
+    "The Jampeta Co operative Urban Bank Ltd",
+    "Jogindra Central Cooperative Bank Ltd",
+    "JALGAON JANATA SAHAKARI BANK LTD JALGAON",
+    "The Jammu & Kashmir Bank Limited",
+    "JAMMU AND KASHMIR GRAMEEN BANK",
+    "Janaseva Sahakari Bank ltd",
+    "Janata Sahakari Bank",
+    "JANA SMALL FINANCE BANK LTD",
+    "The Kaira District Central Co-Op. Bank Ltd.",
+    "Kolhapur Dist. Central Co-Op. Bank Ltd.",
+    "Kalyan Janata Sahakari Bank",
+    "The Kalupur Commercial Co-Op Bank Ltd",
+    "KERALA GRAMIN BANK",
+    "Kotak Mahindra Bank",
+    "THE KANGRA CENTRAL CO-OPERATIVE BANK LTD",
+    "The Karnavati Co operative Bank Ltd",
+    "The Kerala State Co-operative Bank Ltd",
+    "Karnataka Bank",
+    "Karur Vysya Bank",
+    "Karnataka Vikas Grameena Bank",
+    "Maharashtra Gramin Bank",
+    "MEGHALAYA RURAL BANK",
+    "Mehsana Urban Co-operative Bank",
+    "MIZORAM RURAL BANK",
+    "THE NAWANAGAR CO OPERATIVE BANK LTD",
+    "The Nasik Merchants Cooperative Bank Ltd",
+    "NSDL Payments Bank Limited",
+    "The Nainital Bank Ltd",
+    "PASCHIM BANGA GRAMIN BANK",
+    "The Panchmahal District Co Operative Bank Ltd",
+    "Karnataka Gramin Bank (Erstwhile Pragathi Krishna Gramin Bank)",
+  ];
+
   let toggleTab = (tab, con) => {
     // Reset Tabs
     let tabs = ["tab0", "tab1", "tab2", "tab3"];
@@ -18,7 +113,7 @@ const Payment = () => {
   };
 
   useEffect(() => {
-    toggleTab("tab1", "c1");
+    toggleTab("tab3", "c3");
   }, []);
 
   return (
@@ -73,6 +168,7 @@ const Payment = () => {
             </div>
           </div>
           <div className="row">
+            {/* COD CONTENT */}
             <div className={`${style.tabContent} col-12`} id="c0">
               <h4 className="mb-4">COD</h4>
               <p className="d-flex align-items-center mb-4">
@@ -91,6 +187,8 @@ const Payment = () => {
                 />
               </form>
             </div>
+
+            {/* CREDIT/ DEBIT CARD CONTENT */}
             <div className={`${style.tabContent} col-12`} id="c1">
               <form className={style.cdCardForm}>
                 <div className="mb-3">
@@ -150,11 +248,123 @@ const Payment = () => {
                 />
               </form>
             </div>
+
+            {/* NET BANKING CONTENT */}
             <div className={`${style.tabContent} col-12`} id="c2">
-              Net Banking
+              <h4 className="mb-4 fw-bold">Select From Popular Banks</h4>
+              <ul className="list-unstyled ms-3">
+                {popularBanks.map((b, inx) => {
+                  return (
+                    <li key={inx} className="mb-3">
+                      <label htmlFor={`selectedBank${inx}`}>
+                        <input
+                          type="radio"
+                          name="selectedBank"
+                          id={`selectedBank${inx}`}
+                          className="form-check-input me-2"
+                        />
+                        <span style={{ fontSize: "18px" }}>{b}</span>
+                      </label>
+                    </li>
+                  );
+                })}
+              </ul>
+              <h4 className="mt-5 fw-bold">Or Select Other Bank</h4>
+              <select name="otherBank" className="form-select my-4">
+                {otherBanks.map((ob, inx) => {
+                  return (
+                    <option value={ob} key={`ob${inx}`}>
+                      {ob}
+                    </option>
+                  );
+                })}
+              </select>
+              <input
+                type="submit"
+                value="MAKE A PAYMENT"
+                className={style.paymentBtn}
+              />
             </div>
+
+            {/* PAYPAL CONTENT */}
             <div className={`${style.tabContent} col-12`} id="c3">
-              Paypal Account
+              <div className="row">
+                <div className="col">
+                  <img
+                    src={payPal}
+                    alt="Paypal Image"
+                    style={{ width: "60%" }}
+                  />
+                  <p className="text-center my-4" style={{ fontSize: "18px" }}>
+                    Important: You will be redirected to PayPal's website <br />{" "}
+                    to securely complete your payment.
+                  </p>
+                  <input
+                    type="submit"
+                    value="CHECKOUT VIA PAYPAL"
+                    className={style.paymentBtn}
+                  />
+                </div>
+                <div className="col">
+                  <form className={style.cdCardForm}>
+                    <div className="mb-3">
+                      <label htmlFor="cardName" className="from-label">
+                        Name on Card
+                      </label>
+                      <input
+                        type="text"
+                        id="cardName"
+                        className="form-control"
+                        placeholder="Nischay K Gadher"
+                      />
+                    </div>
+
+                    <div className="mb-3">
+                      <label htmlFor="cardName" className="from-label">
+                        Card Number
+                      </label>
+                      <input
+                        type="number"
+                        id="cardNumber"
+                        className="form-control"
+                        placeholder="&#x2022;&#x2022;&#x2022;&#x2022; &#x2022;&#x2022;&#x2022;&#x2022; &#x2022;&#x2022;&#x2022;&#x2022; &#x2022;&#x2022;&#x2022;&#x2022;"
+                      />
+                    </div>
+
+                    <div className="mb-3">
+                      <label htmlFor="cardName" className="from-label">
+                        CVV
+                      </label>
+                      <input
+                        type="number"
+                        id="cvv"
+                        className="form-control"
+                        placeholder="&#x2022;&#x2022;&#x2022;"
+                      />
+                    </div>
+
+                    <div className="mb-5">
+                      <label htmlFor="cardName" className="from-label">
+                        Expiration Date
+                      </label>
+                      <input
+                        type="month"
+                        id="expiryDate"
+                        className="form-control"
+                        min="2000-01"
+                        max="2030-01"
+                        value="2000-01"
+                      />
+                    </div>
+
+                    <input
+                      type="submit"
+                      value="MAKE A PAYMENT"
+                      className={style.paymentBtn}
+                    />
+                  </form>
+                </div>
+              </div>
             </div>
           </div>
         </div>
